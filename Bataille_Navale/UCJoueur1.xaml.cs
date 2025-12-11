76822894a8586f9e4e56a8148e7cdb50854456b0
reboisn <noah.rebois@etu.univ-smb.fr>
2025-12-11 14:18:11 +0100

    Modification de la méthode UnBouton_Click + donne tag au case de la grille + empeche de cliquer sur la grille défense

--- Bataille_Navale/UCJoueur1.xaml.cs
+++ Bataille_Navale/UCJoueur1.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Bataille_Navale
 {
@@ -20,7 +21,8 @@ namespace Bataille_Navale
     /// </summary>
     public partial class UCJoueur1 : UserControl
     {
-        public Button[] lesBoutons = new Button[81];
+        public Button[] lesBoutonsAtt = new Button[81];
+        public Button[] lesBoutonsDef = new Button[81];
         public UCJoueur1()
         {
             InitializeComponent();
@@ -29,42 +31,48 @@ namespace Bataille_Navale
         }
         public void InitialiseGrilleAttaque()
         {
-            for (int i = 0; i < lesBoutons.Length; i++)
+            for (int i = 0; i < lesBoutonsAtt.Length; i++)
             {
-                lesBoutons[i] = new Button();
-                lesBoutons[i].Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Images/carreaux_normal.png", UriKind.Absolute)));
-                lesBoutons[i].Width = 50;
-                lesBoutons[i].Height = 50;
-                lesBoutons[i].VerticalAlignment = VerticalAlignment.Top;
-                lesBoutons[i].HorizontalAlignment = HorizontalAlignment.Left;
-                lesBoutons[i].Margin = new Thickness(lesBoutons[i].Height * (i % 9), lesBoutons[i].Width * (i / 9), 0, 0);
-                this.Grille.Children.Add(lesBoutons[i]);
-                Grid.SetColumn(lesBoutons[i], 0);
+                lesBoutonsAtt[i] = new Button();
+                lesBoutonsAtt[i].Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Images/carreaux_normal.png", UriKind.Absolute)));
+                lesBoutonsAtt[i].Width = 50;
+                lesBoutonsAtt[i].Height = 50;
+                lesBoutonsAtt[i].VerticalAlignment = VerticalAlignment.Top;
+                lesBoutonsAtt[i].HorizontalAlignment = HorizontalAlignment.Left;
+                lesBoutonsAtt[i].Margin = new Thickness(lesBoutonsAtt[i].Height * (i % 9), lesBoutonsAtt[i].Width * (i / 9), 0, 0);
+                this.Grille.Children.Add(lesBoutonsAtt[i]);
+                lesBoutonsAtt[i].Tag = 0;
+                Grid.SetColumn(lesBoutonsAtt[i], 1);
                 // ici il est placé dans la 1ere colonne da ma grille
-                lesBoutons[i].Click += this.UnBouton_Click;
+                lesBoutonsAtt[i].Click += this.UnBouton_Click;
             }
         }
         public void InitialiseGrilleDefense()
         {
-            for (int i = 0; i < lesBoutons.Length; i++)
+            for (int i = 0; i < lesBoutonsDef.Length; i++)
             {
-                lesBoutons[i] = new Button();
-                lesBoutons[i].Width = 50;
-                lesBoutons[i].Height = 50;
-                lesBoutons[i].VerticalAlignment = VerticalAlignment.Top;
-                lesBoutons[i].HorizontalAlignment = HorizontalAlignment.Left;
-                lesBoutons[i].Margin = new Thickness(lesBoutons[i].Height * (i % 9), lesBoutons[i].Width * (i / 9), 0, 0);
-                this.Grille.Children.Add(lesBoutons[i]);
-                Grid.SetColumn(lesBoutons[i], 1);
+                lesBoutonsDef[i] = new Button();
+                lesBoutonsDef[i].Background = new ImageBrush(new BitmapImage(new Uri("pack://application:,,,/Images/carreaux_normal.png", UriKind.Absolute)));
+                lesBoutonsDef[i].Width = 50;
+                lesBoutonsDef[i].Height = 50;
+                lesBoutonsDef[i].VerticalAlignment = VerticalAlignment.Top;
+                lesBoutonsDef[i].HorizontalAlignment = HorizontalAlignment.Left;
+                lesBoutonsDef[i].Margin = new Thickness(lesBoutonsDef[i].Height * (i % 9), lesBoutonsDef[i].Width * (i / 9), 0, 0);
+                this.Grille.Children.Add(lesBoutonsDef[i]);
+                lesBoutonsDef[i].Tag = 0;
+                Grid.SetColumn(lesBoutonsDef[i], 0);
                 // ici il est placé dans la 2eme colonne da ma grille
-                lesBoutons[i].Click += this.UnBouton_Click;
             }
         }
 
-        private void UnBouton_Click(object sender, RoutedEventArgs e)
+       private void UnBouton_Click(object sender, RoutedEventArgs e)
         {
             Button bouton = ((Button)sender);
-            bouton.IsEnabled = false;
+            if (bouton.Tag is 0)
+            {
+                bouton.Background = new ImageBrush(new BitmapImage(new Uri("P:\\SAE1.01 + SAE1.02\\11_12_2025\\Bataille_Navale\\images\\carreaux_raté.png")));
+            }
+            
         }
     }
 }
